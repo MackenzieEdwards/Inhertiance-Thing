7c807bd7a0a18894011f95b44095d7cfe8a5e739
mackenzie.edwards <mackenzie.edwards@MU-C3-18.mullen.pvt.k12.co.us>
2014-11-18 09:07:47 -0700

    school

---
+++ Inhertiance Thing/src/Animal.java
@@ -0,0 +1,26 @@
+public class Animal
+	{
+	protected String name;
+	protected String food;
+	
+	public void makesNoise()
+		{
+		System.out.println("An animal makes noise...");
+		}
+	
+	public void bearsYoung()
+		{
+		System.out.println("An animal reacts to man...");
+		}
+	
+	public void eats()
+		{
+		System.out.println("An animal reacts to man...");
+		}
+	
+	public void flies()
+		{
+		System.out.println("An animal reacts to man...");
+		}
+		
+	}
